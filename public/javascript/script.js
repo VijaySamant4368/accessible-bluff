c7ad23139d4b646f48090948096ce3d5e792fa75
Aswathy <aswathypalakkad69848@gmail.com>
2023-06-20 13:54:32 +0530

    Keyboard shortcuts added, Made screen reader friendly

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -6,6 +6,23 @@
   // To prevent adding listeners while sorting
   var listenNodeInserted = true;
 
+  function notifyScreenReader(text, priority) {
+    var el = document.createElement("div");
+    var id = "speak-" + Date.now();
+    el.setAttribute("id", id);
+    el.setAttribute("aria-live", priority || "polite");
+    //el.classList.add("visually-hidden");
+    document.body.appendChild(el);
+
+    window.setTimeout(function () {
+      document.getElementById(id).innerHTML = text;
+    }, 100);
+
+    window.setTimeout(function () {
+        document.body.removeChild(document.getElementById(id));
+    }, 10000);
+}
+
   const card_container = document.getElementById('card-container');
   card_container.addEventListener("DOMNodeInserted", function(){
 
@@ -16,7 +33,7 @@
         
         ['click','keydown'].forEach( function(evt) {
 
-          card_container.lastChild.addEventListener(evt, (event) => {
+          card_container.lastElementChild.addEventListener(evt, (event) => {
 
             if(evt == 'click' || (evt == 'keydown' && event.keyCode == 13) ){
 
@@ -27,8 +44,12 @@
               const newCard2 = document.createElement('div');
               newCard2.className = 'col-4 col-sm-2 col-lg-1 offset-lg-0 cards ';
               newCard2.id = card.id;
-              newCard2.setAttribute('tabindex', '0');
+              newCard2.setAttribute('tabindex', '1');
+              newCard2.setAttribute('role', 'button');
+              newCard2.setAttribute('title', 'on selected');
               newCard2.textContent = card.textContent;
+
+              notifyScreenReader("Card "+card.textContent+" added.")
 
               selectionContainer.appendChild(newCard2);
 
@@ -57,8 +78,12 @@
           const newCard2 = document.createElement('div');
           newCard2.className = 'col-4 col-sm-2 col-lg-1 offset-lg-0 cards ';
           newCard2.id = card.id;
-          newCard2.setAttribute('tabindex', '0');
+          newCard2.setAttribute('tabindex', '1');
+          newCard2.setAttribute('role', 'button');
+          newCard2.setAttribute('title', 'on deck');
           newCard2.textContent = card.textContent;
+
+          notifyScreenReader("Card "+card.textContent+" removed.")
 
           card_container.appendChild(newCard2);
 
@@ -76,29 +101,54 @@
   });
 
 
-  
-  const passButton = document.getElementById('pass-btn');
 
-document.addEventListener('keydown', (event) => {
-  // Check if the pressed key is either "P" or "p" (case-insensitive)
-  if (event.key.toLowerCase() === 'p') {
-    passButton.click(); // Trigger pass button click event
-  }
-});
+  document.addEventListener('keydown', (event) => {
+    if (event.key.toLowerCase() === ';') {
+      passaction();
+    }
+    else if (event.key.toLowerCase() === 'r') {
+      raisecards();
+    }
+    else if (event.key.toLowerCase() === 'p') {
+      placeCards();
+    }
+    else if (event.key.toLowerCase() === 'f') {
+      card_container.firstChild.focus();
+    }
+    else if (event.key.toLowerCase() === 'j') {
+      card_container.lastChild.focus();
+    }
+    else if (event.key.toLowerCase() === 'd') {
+      placing_container.firstElementChild.focus();
+    }
+    else if (event.key.toLowerCase() === 'k') {
+      placing_container.lastChild.focus();
+    }
+    else if (event.key.toLowerCase() === 's') {
+      const playedContainer = document.getElementById('container_played');
+      playedContainer.firstElementChild.focus();
+    }
+    else if (event.key.toLowerCase() === 'l') {
+      const playedContainer = document.getElementById('container_played');
+      playedContainer.lastChild.focus();
+    }
+    else if (event.key.toLowerCase() === 'h'){
+      var messageSelectedCards = "Selected cards : ";
+      for (var i=0; i<placing_container.children.length; i++) {
+        messageSelectedCards = messageSelectedCards.concat(placing_container.children[i].textContent+" ");
+      }
+      notifyScreenReader(messageSelectedCards);
+    }
 
-const raiseButton = document.getElementById('raise-btn');
 
-document.addEventListener('keydown', (event) => {
-  // Check if the pressed key is either "R" or "r" (case-insensitive)
-  if (event.key.toLowerCase() === 'r') {
-    raiseButton.click(); // Trigger raise button click event
-  }
-});
+
+    return true;
+  });
 
   socket.on('STOC-SET-NUMBER-OF-PLAYERS', (total) => {
     const playerContainer = document.getElementById('player-container')
     for (i = 1; i <= total; i++) {
-      playerContainer.innerHTML += `<div class="user p-1"><i class="far fa-user"></i><span id="user${i}">user${i}</span></div>`
+      playerContainer.innerHTML += `<div class="user p-1" tabindex="0"><i class="far fa-user"></i><span id="user${i}">user${i}</span></div>`
     }
   })
 
@@ -108,6 +158,7 @@ document.addEventListener('keydown', (event) => {
     player.innerHTML = ''
     player.innerText = 'You'
     console.log("playorder:", index);
+    notifyScreenReader("You are in position : "+index, "polite");
   });
 
   function startShufflingEffect() {
@@ -133,6 +184,7 @@ document.addEventListener('keydown', (event) => {
 
   socket.on('STOC-SHUFFLING', (data) => {
     console.log(data)
+    notifyScreenReader("Card is shuffling!");
     startShufflingEffect();
   });
 
@@ -183,7 +235,9 @@ document.addEventListener('keydown', (event) => {
       const newCard = document.createElement('div');
       newCard.className = 'col-4 col-sm-2 col-lg-1 offset-lg-0 cards ';
       newCard.id = card.suit + card.value;
-      newCard.setAttribute('tabindex', '0');
+      newCard.setAttribute('tabindex', '1');
+      newCard.setAttribute('role', 'button');
+      newCard.setAttribute('title', 'on deck');
       newCard.textContent = card.suit + card.value;
 
       // Append the card to the card container;
@@ -192,6 +246,8 @@ document.addEventListener('keydown', (event) => {
     listenNodeInserted = false;
     sort_cards('card-container');
     listenNodeInserted = true;
+
+    notifyScreenReader("Cards received!", "assertive");
   });
 
   socket.on('STOC-SET-WHOS-TURN', (nextPlayerPosition) => {
@@ -204,22 +260,26 @@ document.addEventListener('keydown', (event) => {
       //Fixme : pass button should not be visible on new game start
       const passBtn = document.getElementById('pass-btn')
       passBtn.disabled = false;
-
+      notifyScreenReader("It's your turn!", "assertive");
     }
     else {
       const placeBtn = document.getElementById('place-btn')
       placeBtn.disabled = true;
       const passBtn = document.getElementById('pass-btn')
       passBtn.disabled = true;
+      notifyScreenReader("It's "+next+" turn!", "assertive");
     }
   });
 
   const placeCards = () => {
     var cardContainer = document.getElementById('card-container');
     var cards_remaining = cardContainer.childElementCount;
-    var input = document.getElementById('input');
-    var bluff_text = input.value
-    input.value = ''
+
+    var bluff_text=prompt("Please enter bluff text","Bluff here");
+    if (bluff_text==null){
+       notifyScreenReader("Canceled!")
+       return;
+    }
 
     const selectionContainer = document.getElementById('container_placing');
     var selectedCards = [];
@@ -234,6 +294,7 @@ document.addEventListener('keydown', (event) => {
     selectionContainer.innerHTML = '';    
     const placeBtn = document.getElementById('place-btn')
     placeBtn.disabled = true;
+    notifyScreenReader("Cards placed!", "assertive");
   }
 
   socket.on('STOC-RAISE-TIME-START', () => {
@@ -242,12 +303,14 @@ document.addEventListener('keydown', (event) => {
       const raiseBtn = document.getElementById('raise-btn')
       raiseBtn.disabled = false;
     }
+    notifyScreenReader("Raise time starts!", "assertive");
   });
 
   socket.on('STOC-RAISE-TIME-OVER', () => {
     console.log("raise time over")
     const raiseBtn = document.getElementById('raise-btn')
     raiseBtn.disabled = true;
+    notifyScreenReader("Raise time over!", "assertive");
   });
 
   socket.on('STOC-GAME-PLAYED', (CardCount, bluffText) => {
@@ -258,11 +321,14 @@ document.addEventListener('keydown', (event) => {
       const newCard = document.createElement('div');
       newCard.className = 'col-4 col-sm-2 col-lg-1 offset-lg-0 cards ';
       newCard.id = bluffText;
-      newCard.setAttribute('tabindex', '0');
+      newCard.setAttribute('tabindex', '1');
+      newCard.setAttribute('role', 'button');
+      newCard.setAttribute('title', 'on played');
       newCard.textContent = bluffText;
       // Append the card to the card container;
       playingContainer.appendChild(newCard);
     }
+    notifyScreenReader("Game played. "+CardCount+" cards played as "+bluffText, "assertive");
   });
 
   function raisecards() {
@@ -281,13 +347,19 @@ document.addEventListener('keydown', (event) => {
       playedContainer.removeChild(playedContainer.lastChild);
     });
     // Create a new element for each popped element 
+    var items = "";
     poppedElements.forEach((element, index) => {
       var cardElement = document.createElement('div');
       cardElement.className = 'col-4 col-sm-2 col-lg-1 offset-lg-0 cards ';
       cardElement.textContent = poppedSuits[index] + element;;
+      items = items+" "+poppedSuits[index] + element;
+      cardElement.setAttribute('tabindex', '1');
+      cardElement.setAttribute('role', 'button');
+      cardElement.setAttribute('title', 'on raised');
       cardElement.style.backgroundColor = "#ff0000";
       playedContainer.appendChild(cardElement);
     });
+    notifyScreenReader("Raiced cards : "+items, "assertive");
   });
 
   socket.on('STOC1C-DUMP-PENALTY-CARDS', (CardStack, poppedElements, SuitStack, poppedSuits) => {
@@ -302,6 +374,8 @@ document.addEventListener('keydown', (event) => {
       cardsGivingBack.push(CardStack.pop());
       suitsBack.push(SuitStack.pop());
     }
+
+    var items = "";
     console.log("cards for faileduser:", cardsGivingBack);
     console.log("suits for faileduser:", suitsBack);
     const cardContainer = document.getElementById('card-container');
@@ -309,8 +383,11 @@ document.addEventListener('keydown', (event) => {
       const newCard = document.createElement('div');
       newCard.className = 'col-4 col-sm-2 col-lg-1 offset-lg-0 cards ';
       newCard.id = suitsBack[index] + cardvalue;
-      newCard.setAttribute('tabindex', '0');
+      newCard.setAttribute('tabindex', '1');
+      newCard.setAttribute('role', 'button');
+      newCard.setAttribute('title', 'on deck');
       newCard.textContent = suitsBack[index] + cardvalue;
+      items = items+" "+suitsBack[index] + cardvalue;
       newCard.style.backgroundColor = "#ff0000";
  
       // Append the card to the card container;
@@ -321,6 +398,8 @@ document.addEventListener('keydown', (event) => {
     listenNodeInserted = false;
     sort_cards('card-container');
     listenNodeInserted = true;
+
+    notifyScreenReader("Recived penalty cards : "+items, "assertive");
   })
 
   function passaction(){
@@ -339,6 +418,9 @@ document.addEventListener('keydown', (event) => {
     player.style.backgroundColor = "#97FF00";
     if(pos === player_position){
       alert("Won!");
+    }
+    else{
+      notifyScreenReader("Player winned : "+player_position, "assertive");
     }
   });
 
